6906011a0b02e201d5bdc1116e5e73e7ee4de700
Daniil <danvol121@gmail.com>
2025-11-28 12:43:57 +0300

    add project code

---
+++ CaspianLevelSea/data/loader.py
@@ -0,0 +1,94 @@
+import logging
+import requests
+from bs4 import BeautifulSoup
+import os
+from pathlib import Path
+
+logging.basicConfig(
+    level=logging.INFO, 
+    filename="log.log", 
+    filemode="w", 
+    format="%(asctime)s %(levelname)s %(message)s", 
+    encoding='utf-8'
+    )
+
+class Loader():
+    '''
+    Class for load raw data from the site
+    '''
+    def __init__(self, parent_url: str, chunk_size: int=8192) -> None:
+        '''
+        Init class for Loader
+        :params:
+            parent_url: str - url for parent page where find data
+            chunk_size: int (default=8192) - the size of data chunk during loading
+        '''
+        self.parent_url = parent_url
+        self.chunk_size = chunk_size
+
+    def load(self, data_path: str):
+        '''
+        :params:
+            data_path: str - path where load data
+        '''
+        data_url = self._find_url_link(self.parent_url)
+        self.__load(url_link=data_url, data_path=data_path, chunk_size=self.chunk_size)
+    
+    def _find_url_link(self, parent_url: str) -> str:
+        '''
+        Get url for actual level sea data from the parent page
+        :params:
+            parent_url: str - url for parent page where find data
+        :returns:
+            data_url: str - url where contain data
+        '''
+        try:
+            # try to connect to parent page
+            response = requests.get(parent_url, timeout=10)
+            response.raise_for_status()
+            logging.debug(response.text)
+            
+            # Find url for data
+            soup = BeautifulSoup(response.text, 'html.parser')
+            link_tag = soup.find('a', href=lambda href: href and 'Sea-level-EN.zip' in href)
+            # check on existance if attribute and get link
+            if link_tag:
+                data_url = link_tag.get('href')
+                logging.info(f"Url for data: {data_url}") 
+            else:
+                data_url = None
+                logging.error(f"URL on Sea-level-EN.zip not found")
+
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error during connection to {parent_url} or during search url: {e}")
+        
+        return data_url
+    
+    def __load(self, url_link: str, data_path: str, chunk_size: int) -> None:
+        '''
+        Load data from url to data_path
+        :params:
+            url_link: str - url link on data of level sea
+            data_path: str - path where save data
+            chunk_size: int - the size of data chunk 
+        '''
+        # create parent directory if not exist
+        path = Path(data_path)
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        # try to connect and save data
+        try:
+            response = requests.get(url_link, stream=True, timeout=30)
+            response.raise_for_status()
+
+            with open(data_path, 'wb') as f:
+                for chunk in response.iter_content(chunk_size=chunk_size):
+                    if chunk:
+                        f.write(chunk)
+            
+            logging.info(f"Level sea data succsesfully saved in {data_path}")
+        
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error during loading data: {e}")
+
+
---
+++ CaspianLevelSea/data/process.py
@@ -0,0 +1,114 @@
+import re
+import pandas as pd
+import sqlite3
+import zipfile
+import io
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, 
+    filename="log.log", 
+    filemode="w", 
+    format="%(asctime)s %(levelname)s %(message)s", 
+    encoding='utf-8'
+    )
+
+class ObjectProcessor():
+    '''
+    Class for process raw data to the database
+    '''
+    def __init__(self):
+        '''
+        Init class for ObjectProcess
+        '''
+        self.metadata_df = []
+        self.metadata_patterns = {
+            'post_name': re.compile(r'post[^_]*_+([^_]+)_+$'),
+            'opening_date': re.compile(r'opening date[^_]*_+([^_]+)_+$'),
+            'latitude': re.compile(r'latitude[^_]*_+([^_]+)_+'),
+            'longitude': re.compile(r'longitude[^_]*_+([^_]+)_+'),
+            'post_datum': re.compile(r'post datum[^_]*_+([^_]+)_+'),
+            'altitude_system': re.compile(r'(?:altitude system[^_]*_+|from the)([^_]+)' ) # r'altitude system[^_]*_+([^_]+)_+' # r'(?:altitude system[^_]*_+|from the)([^_]+)' 
+        }
+        self.year_pattern = re.compile(r'^\d{4,}$')
+
+    def process_object(self, raw_data_path: str, object_path: str, metadata_path: str) -> None:
+        '''
+        Method for process every post, parse metadata and level sea
+        :params:
+            raw_data_path: str - path to raw zip file with posts
+            object_path: str - path to object storage where save data
+            metadata_path: str - path to metadata storage
+        '''
+        # Make connection to sqlite server
+        self.conn = sqlite3.connect(object_path)
+
+        # Read data
+        with zipfile.ZipFile(raw_data_path, 'r') as self.zf:
+            xls_files = [f for f in self.zf.namelist() if f.endswith(('.xls', '.xlsx'))]
+
+            # iterate over posts in zip
+            for file_name in xls_files:
+                self._process_file(file_name)
+
+            pd.DataFrame(self.metadata_df).to_csv(metadata_path, index=False)
+        self.conn.close()
+    
+    def _process_file(self, file_name) -> None:
+        '''
+        Method for process one file (post) and save result in database
+        '''
+        post_file = self._get_post_name(file_name)
+        meta_info = {key: None for key in self.metadata_patterns}
+
+        # Open file
+        with self.zf.open(file_name) as zip_file:
+            df = pd.read_excel(io.BytesIO(zip_file.read()), header=None)
+        
+        df_ = pd.DataFrame(columns=['datetime', 'sea_level'])
+        current_index = 0
+        # Iterate over dataframe: parse metadata and data
+        for i in range(len(df)):
+            s_lower = str(df.iloc[i,0]).strip().lower()
+            for key, pattern in self.metadata_patterns.items():
+                if meta_info[key] is None:
+                    match = pattern.search(s_lower)
+                    if match:
+                        meta_info[key] = match.group(1)
+            
+            match = re.search(self.year_pattern, s_lower)
+            if match: 
+                year = df.iloc[i,0]
+                for month in range(1,13):
+                    df_.loc[current_index,'datetime'] = f'{month}-{year}'
+                    df_.loc[current_index,'sea_level'] = df.iloc[i,month]
+                    current_index += 1
+        
+        self.metadata_df.append(meta_info)
+        df_.to_sql(name=post_file, con=self.conn, if_exists='replace', index=False)
+
+        logging.info(f"Succesfully save post: {post_file}")
+
+    def _get_post_name(self, file_name: str) -> str:
+        '''
+        Method for extract name of post from the file name.
+        For example:
+        'Sea-level-EN/AVERAGE/TUL_av.xls' -> 'TUL'
+
+        First search lettres before '_av.'
+        After filter symbols ['(', ')', ' '] and shange them on ''
+
+        :params:
+            file_name: str - path to the file
+        :returns:
+            post_name: str - name of the post
+        '''
+        # Find lettres before '_av.'
+        name_pattern = r'[A-Za-z0-9_\(\) ]+(?=_av\.)'
+        post_name = re.findall(name_pattern, file_name)[-1]
+
+        # Filter symbols ['(', ')', ' ']
+        special_symbols_pattern = r'[\(\)\s]'
+        post_name = re.sub(special_symbols_pattern, '', post_name)
+
+        return post_name
